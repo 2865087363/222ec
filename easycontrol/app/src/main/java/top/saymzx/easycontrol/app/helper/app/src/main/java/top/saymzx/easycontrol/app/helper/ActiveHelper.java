a0cb4016cea95fe3401b2769a59aa973e4ee9cd2
2865087363 <2865087363@qq.com>
2026-05-05 22:09:13 +0800

    Create ActiveHelper.java

---
+++ easycontrol/app/src/main/java/top/saymzx/easycontrol/app/helper/app/src/main/java/top/saymzx/easycontrol/app/helper/ActiveHelper.java
@@ -0,0 +1,14 @@
+package top.saymzx.easycontrol.app.helper;
+
+import top.saymzx.easycontrol.app.entity.AppData;
+
+public class ActiveHelper {
+
+    public static boolean active(String key) {
+        return true;
+    }
+
+    public static boolean deactivate(String key) {
+        return true;
+    }
+}
